aac752642106c3b0bb1e394670be70049ba346c9
Kim Nguyen <kimnivore@gmail.com>
2022-02-23 12:33:28 -0800

    exercise 5

--- index.js
+++ index.js
@@ -86,7 +86,8 @@ class Seasons {
    * [Exercise 5A] Seasons creates a seasons object
    */
   constructor() {
-    // ✨ initialize whatever properties are needed
+    this.seasons = ['summer', 'fall', 'winter', 'spring'];
+    this.currentSeason = 0;
   }
 
   /**
@@ -102,7 +103,13 @@ class Seasons {
    * seasons.next() // returns "summer"
    */
   next() {
-    // ✨ implement
+    const result = this.seasons[this.currentSeason]
+    if(this.currentSeason === 3) {
+      this.currentSeason = 0
+    }else{
+      ++this.currentSeason
+    }
+    return result
   }
 }
 
